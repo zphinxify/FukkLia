b267e7d082dc185abaf44eaeb347825bf375b875
zphinxify <benjamiiiin94rgmail.com>
2020-09-25 11:41:16 +0200

    first

---
+++ LiaFind.Core/Data/LiaFindCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LiaFind.Core.Models;
+
+namespace LiaFind.Core.Data
+{
+    public class LiaDbContext : DbContext
+    {
+        public LiaDbContext (DbContextOptions<LiaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ LiaFind.Core/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiaFind.Core.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentEmail { get; set; }
+        public float StudentAge { get; set; }
+    }
+}
